dadf8f97857b4c3965fbebbacf65d451ecce1db6
tinoslabs <59659322+tinoslabs@users.noreply.github.com>
2025-01-04 23:32:42 +0530

    Update settings.py

--- nexhirepro/settings.py
+++ nexhirepro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3redbt$9^g1-i=q(t)y0#u7ut(d^i6iwygue@uxwvb=g^^3+^3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['nexhire-1.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
